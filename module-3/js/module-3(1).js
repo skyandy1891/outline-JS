62e1d7974aa4038678f28c9e96d6fe0a7c6240ae
skyandy1891 <skyandy1891@gmail.com>
2022-04-13 21:01:02 +0300

    update module 3(1)/ task 30

--- module-3/js/module-3(1).js
+++ module-3/js/module-3(1).js
@@ -140,7 +140,7 @@
 
 // =========================== hw ===========================
 
-// task 6
+                           // task 6
 // Дополни код обновив значения свойств объекта apartment:
 
 // цену в свойстве price на 5000;
@@ -167,7 +167,8 @@
 //     console.log(apartment);
     // ============ done ============
 
-// task 7
+                            // task 7
+
 // Добавь объекту apartment несколько новых свойств:
 
 // area - площадь в квадратных метрах, число 60;
@@ -195,7 +196,7 @@
 //     }
     // ========== done ==========
 
-// task 8
+                             // task 8
 // Дополни код объявления объекта так, чтобы у него были свойства name, price, image и tags со значениями из переменных с аналогичными именами. Обязательно используй синтаксис коротких свойств.
 
 // const name = "Repair Droid";
@@ -212,7 +213,7 @@
     
     // ============ done ============
 
-// task 9
+                            // task 9
 // Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства: email и password, имена которых хранятся в переменных emailInputName и passwordInputName.
 // Значением свойства email должна быть строка "henry.carter@aptmail.com", а значением свойства password - строка "jqueryismyjam".
 
@@ -227,7 +228,7 @@
 // };
 // ============ done ============
 
-// task 10
+                              // task 10
 // Перебери объект apartment используя цикл for...in и запиши в массив keys все его ключи, а в массив values все значения его свойств.
 
 // const apartment = {
@@ -243,7 +244,7 @@
 //     }
     // ============ done ============
 
-    // task 11
+                              // task 11
     // Выполни рефакторинг решения предыдущего задания добавив в цикл for...in проверку на собственное свойство.
 
 //     const keys = [];
@@ -264,7 +265,7 @@
 // }
 // ============ done ============
 
-    // task 12
+                            // task 12
 // Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object. Используй переменную propCount для хранения количества свойств объекта.
 
 // function countProps(object) {
@@ -279,7 +280,7 @@
 //   }
 // ============ done ============
 
-    // task 13
+                              // task 13
     // Перебери объект apartment используя метод Object.keys() и цикл for...of. Запиши в переменную keys массив ключей собственных свойств объекта apartment, и добавь в массив values все значения его свойств.
 
     // const apartment = {
@@ -295,20 +296,364 @@
     //   }
 // ============ done ============
     
-    // task 14
+                               // task 14
     // Выполни рефакторинг функции countProps(object) используя метод Object.keys() и, возможно, но необязательно, цикл for...of.
 
     // function countProps(object) {
     //     // Change code below this line
-    //     let propCount = 0;
-      
-    //     for (const key in object) {
-    //       if (object.hasOwnProperty(key)) {
+        
+    //     // for (const key in object) {
+    //     //   if (object.hasOwnProperty(key)) {
+    //     //     propCount += 1;
+    //     //   }
+    //     // }
+
+    //   let propCount = 0;
+    //     const keys = Object.keys(object);
+    //     for(const key of keys){
     //         propCount += 1;
-    //       }
     //     }
-      
     //     return propCount;
     //     // Change code above this line
     //   }
+      
     // ============ done ============   
+
+                               // task 15
+    // Запиши в переменную keys массив ключей собственных свойств объекта apartment, а в переменную values массив всех значений его свойств. Используй методы Object.keys() и Object.values().
+
+    //  const apartment = {
+    //      descr: "Spacious apartment in the city center",
+    //         rating: 4,
+    //         price: 2153,
+    //         };
+    //  const keys = Object.keys(apartment);
+    //  const values = Object.values(apartment);
+
+    // ============ done ============
+
+                               // task 16
+// Напиши функцию countTotalSalary(salaries) которая принимает объект зарплат, где имя свойства это имя сотрудника, а значение свойства это зарплата. Функция должна рассчитать общую сумму зарплат сотрудников и вернуть её. Используй переменную totalSalary для хранения общей суммы зарплаты.
+
+// function totalSalary(salaries) {
+//     let totalSalary = 0;
+//     for (const key in salaries) {
+//         if (salaries.hasOwnProperty(key)) {
+//             totalSalary += salaries[key];
+//         }
+//     }
+
+//     return totalSalary;
+// }
+
+// ============ done ============
+
+                                // task 17
+// Перебери массив объектов colors используя цикл for...of. Добавь в массив hexColors значения свойств hex, а в массив rgbColors значения свойств rgb из всех объектов массива colors.
+
+//   const colors = [
+//     { hex: "#f44336", rgb: "244,67,54" },
+//     { hex: "#2196f3", rgb: "33,150,243" },
+//     { hex: "#4caf50", rgb: "76,175,80" },
+//     { hex: "#ffeb3b", rgb: "255,235,59" },
+//   ];
+  
+//   const hexColors = [];
+//   const rgbColors = [];
+
+//     for(const color of colors){
+//         hexColors.push(color.hex);
+//         rgbColors.push(color.rgb);
+//     }
+    // ============ done ============
+
+                             // task 18
+// Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). Если продукт с таким названием не найден, функция должна возвращать null.
+    
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getProductPrice(productName) {
+//     // Change code below this line
+//   for(const product of products){
+//     let name = product.name;
+//     if(name === productName){
+//       return product.price;
+//     }
+//    }
+//    return null;
+//   }
+//   ============ done ============
+
+                                  // task 19
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр propName - имя (ключ) свойства. Функция должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
+
+// const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+  
+//   function getAllPropValues(propName) {
+//     // Change code below this line
+//     const array = [];
+//   for(const product of products){
+//   if (product[propName]) {
+//         array.push(product[propName]);
+//       }
+//     }
+//     return array;
+//   }
+//   ============ done ============
+
+                                     // task 20
+    // Напиши функцию calculateTotalPrice(productName) которая принимает один параметр productName - название товара. Функция должна вернуть общую стоимость (цена * количество) товара с таким именем из массива products.
+
+    // const products = [
+    //     { name: "Radar", price: 1300, quantity: 4 },
+    //     { name: "Scanner", price: 2700, quantity: 3 },
+    //     { name: "Droid", price: 400, quantity: 7 },
+    //     { name: "Grip", price: 1200, quantity: 9 },
+    //   ];
+
+    //   function calculateTotalPrice(productName) {
+    //     let total = 0;
+    //     for (const prop of products) {
+    //       let name = prop.name;
+    //       if (productName === name) {
+    //         total = prop.price * prop.quantity;
+    //       }
+    //     }
+    //     return total;
+    //   }
+    //   ============ done ============
+
+                                   // task 21
+    // Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю температуру за три дня (meanTemperature). Замени объявления переменных yesterday, today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures.
+
+    // const highTemperatures = {
+    //     yesterday: 28,
+    //     today: 26,
+    //     tomorrow: 33,
+    //   };
+    //   // Change code below this line
+    //   const {yesterday, today, tomorrow} = highTemperatures;
+    //   // Change code above this line
+    //   const meanTemperature = (yesterday + today + tomorrow) / 3;
+      
+//  ============ done ===============
+
+                                   // task 22
+    // В прогнозе максимальных температур также может быть необязательное свойство icon - иконка погоды. Замени объявления переменных yesterday, today, tomorrow и icon одной операцией деструктуризации свойств объекта highTemperatures. Задай значение по умолчанию для icon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+    // const highTemperatures = {
+    //     yesterday: 28,
+    //     today: 26,
+    //     tomorrow: 33,
+    //   };
+      
+    //   const {yesterday, today, tomorrow, icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg"} = highTemperatures;
+      
+    //   const meanTemperature = (yesterday + today + tomorrow) / 3;
+      
+//  ============ done ===============
+
+                                   // task 23
+    // Замени объявления переменных highYesterday, highToday, highTomorrow и highIcon одной операцией деструктуризации свойств объекта highTemperatures. Задай значение по умолчанию для highIcon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+    // const highTemperatures = {
+    //     yesterday: 28,
+    //     today: 26,
+    //     tomorrow: 33,
+    //   };
+      // Change code below this line
+      
+      // const highYesterday = highTemperatures.yesterday;
+      // const highToday = highTemperatures.today;
+      // const highTomorrow = highTemperatures.tomorrow;
+      // const highIcon = highTemperatures.icon;
+      
+    //   const {
+    //     yesterday: highYesterday,
+    //     today: highToday,
+    //     tomorrow: highTomorrow,
+    //     icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+    //   } = highTemperatures;
+      // Change code above this line
+    //   const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+    //   ============ done ===============
+
+                                    // task 24
+    // Выполни рефакторинг цикла for...of так, чтобы в нём использовалась деструктуризация объекта..
+
+    // const colors = [
+    //     { hex: "#f44336", rgb: "244,67,54" },
+    //     { hex: "#2196f3", rgb: "33,150,243" },
+    //     { hex: "#4caf50", rgb: "76,175,80" },
+    //     { hex: "#ffeb3b", rgb: "255,235,59" },
+    //   ];          
+      
+    //   const hexColors = []; // массив с кодами цветов в шестнадцатеричном представлении
+    //   const rgbColors = [];
+
+    // //   for(const color of colors)
+    //   for (const {hex, rgb} of colors) {
+    //     hexColors.push(colors.hex)  
+    //     hexColors.push(hex); // добавляем в массив hexColors значения свойства hex из каждого элемента массива colors
+    //     rgbColors.push(colors.rgb); 
+    //     rgbColors.push(rgb);  // добавляем в массив rgbColors значения свойства rgb из каждого элемента массива colors
+    //   }
+
+   
+//  ============ done ===============
+
+                                // task 25
+    // Мы получили прогноз погоды на два дня, с минимальными и максимальными температурами, а также необязательными иконками. Замени объявления всех переменных одной операцией деструктуризации свойств объекта forecast. Задай значение по умолчанию для иконок, переменных todayIcon и tomorrowIcon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+    // const forecast = {
+    //     today: {
+    //       low: 28,
+    //       high: 32,
+    //       icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+    //     },
+    //     tomorrow: {
+    //       low: 27,
+    //       high: 31,
+    //     },
+    //   };
+
+    //  const highToday = forecast.today.high;
+    //   const lowToday = forecast.today.low;
+    //   const todayIcon = forecast.today.icon;
+    
+    //   const highTomorrow = forecast.tomorrow.high;
+    //   const lowTomorrow = forecast.tomorrow.low;
+    //   const tomorrowIcon = forecast.tomorrow.icon;
+
+
+      // Change code below this line
+        // const {
+        //     today: {
+        //         low: lowToday,
+        //         high: highToday,
+        //         icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+        //     },
+        //     tomorrow: {
+        //         low: lowTomorrow,
+        //         high: highTomorrow,
+        //         icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+        //     },
+        // } = forecast;
+
+//  ============ done ===============
+
+                                // task 26                          
+// Функция calculateMeanTemperature(forecast) принимает один параметр forecast - объект температур на два дня следующего формата.
+// {
+//     today: { low: 10, high: 20 },
+//   tomorrow: { low: 20, high: 30 }
+// }
+// Замени объявления переменных todayLow, todayHigh, tomorrowLow и tomorrowHigh одной операцией деструктуризации свойств объекта forecast.
+
+// function calculateMeanTemperature(forecast) {
+//     // const todayLow = forecast.today.low;
+//     // const todayHigh = forecast.today.high;
+//     // const tomorrowLow = forecast.tomorrow.low;
+//     // const tomorrowHigh = forecast.tomorrow.high;    
+  
+//     const{
+//         today: {low: todayLow, high: todayHigh},
+//         tomorrow: {low: tomorrowLow, high: tomorrowHigh}
+//       } = forecast;
+
+//     // Change code above this line
+//     return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+//   }
+
+//  ============ done ==============
+
+
+                                // task 27                  
+// В переменной scores хранится массив результатов тестирования. Используя распыление и методы Math.max() и Math.min() дополни код так, чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
+
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
+
+//  ============ done ==============
+
+
+                                // task 28  
+// В переменных firstGroupScores, secondGroupScores и thirdGroupScores хранятся результаты тестирования отдельных групп. Используя распыление дополни код так, чтобы:
+// В переменной allScores хранился массив всех результатов от первой до третьей группы.
+// В переменной bestScore был самый высокий общий балл.
+// В переменной worstScore был самый низкий общий балл.
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+// Change code below this line
+const allScores = [...firstGroupScores, ...secondGroupScores,...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+// ============ done ==============
+
+
+                                // task 29              
+// В конструкторе можно создавать новые тесты, для которых есть настройки по умолчанию которые хранятся в переменной defaultSettings. Во время создания теста, все или часть настроек можно переопределить, они хранятся в переменной overrideSettings.
+// Для того чтобы получить финальные настройки теста, необходимо взять настройки по умолчанию и поверх них применить переопределённые настройки. Дополни код так, чтобы в переменной finalSettings получился объект финальных настроек теста.
+
+// const defaultSettings = {
+//     theme: "light",
+//     public: true,
+//     withPassword: false,
+//     minNumberOfQuestions: 10,
+//     timePerQuestion: 60,
+//   };
+//   const overrideSettings = {
+//     public: false,
+//     withPassword: true,
+//     timePerQuestion: 30,
+//   };
+//   // Change code below this line
+//   const finalSettings = {...defaultSettings, ...overrideSettings};
+
+// ============ done ==============
+
+
+                                // task 30  
+// Напиши функцию makeTask(data) которая принимает один параметр data - объект со следующими свойствами.
+
+// text - текст задачи.
+// category - категория задачи.
+// priority - приоритет задачи.
+// Функция должна составить и вернуть новый объект задачи, не изменяя напрямую параметр data. В новом объекте должно быть свойство completed, значение которого хранится в одноимённой локальной переменной.
+
+// В параметре data гарантированно будет только свойство text, а остальные два, category и priority, могут отсутствовать. Тогда, в новом объекте задачи, в свойствах category и priority должны быть значения по умолчанию, хранящиеся в одноимённых локальных переменных.
+
+// function makeTask(data) {
+//     const completed = false;
+//     const category = "General";
+//     const priority = "Normal";
+//     // Change code below this line
+//   const newDate = {completed, category, priority, ...data};
+//   return newDate;
+//     // Change code above this line
+//   }
+
+// ============ done ==============
+
+
+     
+    
+      
+      
+        
